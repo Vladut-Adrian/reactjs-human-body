6396e8b4b43aeff4f26973d07f7c4b42c8751fa4
Vladut Pavel <pavelvladutadrian@gmail.com>
2023-03-29 00:39:08 +0300

    added dist and removed dist from gitignore

---
+++ dist/components/BodyComponent/BodyComponent.d.ts
@@ -0,0 +1,9 @@
+import React from "react";
+export declare const SVG_PARTS: any;
+export declare const PARTS_GROUPS: any;
+export interface BodyComponentProps {
+    onClick?: Function;
+    onChange?: Function;
+    partsInput?: any;
+}
+export declare const BodyComponent: React.FC<BodyComponentProps>;
